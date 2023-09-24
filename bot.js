52a6a787f5c53ac6a61f030dc25ed1e757e2afc9
HanSaloZu <conswintcomin@gmail.com>
2023-09-24 23:33:48 +0300

    Initialize chat bot and service app

---
+++ bot.js
@@ -0,0 +1,22 @@
+import { HearManager } from "@vk-io/hear";
+import config from "config";
+import { VK } from "vk-io";
+import db from "./db";
+import Peer from "./models/Peer";
+import Topic from "./models/Topic";
+
+const service = new VK({
+  token: config.get("serviceToken")
+});
+
+const bot = new VK({
+  token: config.get("communityToken"),
+  apiLimit: 18
+});
+
+const hearManager = new HearManager();
+
+bot.updates.on("message_new", hearManager.middleware);
+
+db.sync({ alter: true });
+bot.updates.start().catch(console.error);
